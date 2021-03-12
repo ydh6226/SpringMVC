2b5a4a8f84143e2c36f0efc871525ff6c5ea0ace
ydh6226 <ydh6226@naver.com>
2021-03-12 23:14:37 +0900

    프론트 컨트롤러 도입, View 분리

---
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -0,0 +1,20 @@
+package hello.servlet.web.frontcontroller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.rmi.ServerException;
+
+public class MyView {
+    private String viewPath;
+
+    public MyView(String viewPath) {
+        this.viewPath = viewPath;
+    }
+
+    public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher(viewPath)
+                .forward(request, response);
+    }
+}
